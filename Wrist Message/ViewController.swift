18bf91bf6b9d2db422bfc09df2a11f3b623e01d8
Tyler Cheves <tyler@tylers-macbook-pro-2.local>
2016-03-11 19:13:51 -0800

    Initial Commit

---
+++ Wrist Message/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wrist Message
+//
+//  Created by Tyler Cheves on 3/11/16.
+//  Copyright © 2016 Healthy Pixels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
